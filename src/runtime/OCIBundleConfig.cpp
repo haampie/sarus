01045d7dab454f52bf187ea325b281f4836fd2f9
Harmen Stoppels <harmenstoppels@gmail.com>
2020-07-11 17:04:53 +0200

    Cannot use cgroups at the moment :( have to figure that one out

--- src/runtime/OCIBundleConfig.cpp
+++ src/runtime/OCIBundleConfig.cpp
@@ -292,7 +292,7 @@ rj::Value OCIBundleConfig::makeMemberLinux() const {
 
         cpu.AddMember("cpus", cpuAffinity, *allocator);
         resources.AddMember("cpu", cpu, *allocator);
-        linux.AddMember("resources", resources, *allocator);
+        // linux.AddMember("resources", resources, *allocator);
     }
     // namespaces
     {
